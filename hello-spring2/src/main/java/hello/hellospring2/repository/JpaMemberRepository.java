ef829eea0a347b10c98ff8097b820dde23816d16
nowonjae <nodnjs1231@naver.com>
2022-06-24 09:32:50 +0900

    JPA 1차

---
+++ hello-spring2/src/main/java/hello/hellospring2/repository/JpaMemberRepository.java
@@ -0,0 +1,41 @@
+package hello.hellospring2.repository;
+
+import hello.hellospring2.domian.Member;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
+
+public class JpaMemberRepository implements MemberRepository{
+
+    //JPA는 EntityManager로 모든게 동작된다.
+    private final EntityManager em;
+
+    public JpaMemberRepository(EntityManager em) {
+        this.em = em;
+    }
+
+
+
+
+
+    @Override
+    public Member save(Member member) {
+        return null;
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return null;
+    }
+}
